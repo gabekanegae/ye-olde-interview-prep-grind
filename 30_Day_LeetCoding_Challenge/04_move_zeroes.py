0330b60daa7b0f939ded5c28fce59f4820c1991e
Kanegae <gabriel.kanegae@usp.br>
2020-04-05 21:35:40 -0300

    Simplify 30-Day-LC #4, #5

--- 30_Day_LeetCoding_Challenge/04_move_zeroes.py
+++ 30_Day_LeetCoding_Challenge/04_move_zeroes.py
@@ -1,14 +1,9 @@
 def moveZeroes(nums):
-    i = 0
-    while i < len(nums) and nums[i] != 0:
-        i += 1
-    j = i+1
-
-    while j < len(nums):
-        if nums[i] == 0 and nums[j] != 0:
-            nums[i], nums[j] = nums[j], nums[i]
-            i += 1
-        j += 1
+    lastNonZero = 0
+    for i in range(len(nums)):
+        if nums[i] != 0:
+            nums[lastNonZero], nums[i] = nums[i], nums[lastNonZero]
+            lastNonZero += 1
 
 nums = [0, 1, 0, 3, 12]
 moveZeroes(nums)
@@ -16,4 +11,4 @@ print(nums) # [1, 3, 12, 0, 0]
 
 nums = [4, 2, 4, 0, 0, 3, 0, 5, 1, 0]
 moveZeroes(nums)
-print(nums) # [4, 2, 4, 0, 0, 3, 0, 5, 1, 0]
+print(nums) # [4, 2, 4, 3, 5, 1, 0, 0, 0, 0]
--- 30_Day_LeetCoding_Challenge/05_best_time_to_buy_and_sell_stock_ii.py
+++ 30_Day_LeetCoding_Challenge/05_best_time_to_buy_and_sell_stock_ii.py
@@ -1,18 +1,12 @@
 def maxProfit(prices):
-    i, j = 0, 1
-
     total = 0
-    while j < len(prices):
-        while j < len(prices) and prices[j] > prices[j-1]:
-            j += 1
-        
-        total += prices[j-1]-prices[i]
-
-        i = j
-        j = j+1
+    for i in range(1, len(prices)):
+        if prices[i] > prices[i-1]:
+            total += prices[i]-prices[i-1]
 
     return total
 
 print(maxProfit([7, 1, 5, 3, 6, 4])) # 7
 print(maxProfit([1, 2, 3, 4, 5])) # 4
 print(maxProfit([7, 6, 4, 3, 1])) # 0
+print(maxProfit([6, 1, 3, 2, 4, 7])) # 7
