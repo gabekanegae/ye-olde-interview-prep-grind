bc748182b8e29a1a33ee257abb9db06056e90edf
Kanegae <gabriel.kanegae@usp.br>
2020-04-08 10:39:55 -0300

    Add LC #108

---
+++ LeetCode/0108_convert_sorted_array_to_bst.py
@@ -0,0 +1,10 @@
+def sortedArrayToBST(nums):
+    if not nums: return None
+
+    mid = len(nums)//2
+
+    root = TreeNode(nums[mid])
+    root.left = self.sortedArrayToBST(nums[:mid])
+    root.right = self.sortedArrayToBST(nums[mid+1:])
+
+    return root
