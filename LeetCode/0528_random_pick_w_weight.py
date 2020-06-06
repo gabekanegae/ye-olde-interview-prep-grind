7f2561029bd0cf8371875aed78b8490bea0f3a19
Kanegae <gabriel.kanegae@usp.br>
2020-06-05 23:22:56 -0300

    More robust testing for LC #528

--- LeetCode/0528_random_pick_w_weight.py
+++ LeetCode/0528_random_pick_w_weight.py
@@ -1,15 +1,27 @@
 from random import randint
 
-def pickIndex(w):
-    acc = []
+class Solution:
+    def __init__(self, w):
+        self.w = w
+        self.acc = []
 
-    r = randint(0, sum(w)-1)
-    
-    acc.append(w[0])
-    for i in range(1, len(w)):
-        acc.append(w[i] + acc[i-1])
+    def pickIndex(self):
+        if not self.acc:
+            self.acc = [self.w[0]]
+            for i in range(1, len(self.w)):
+                self.acc.append(self.w[i] + self.acc[i-1])
         
-    for i, v in enumerate(acc):
-        if r < v: return i
+        r = randint(0, self.acc[-1]-1)
 
-print(pickIndex([0, 1, 2, 3, 4]))
+        for i, v in enumerate(self.acc):
+            if r < v: return i
+
+n = 1000000
+weights = [0, 1, 2, 3, 4]
+sol = Solution(weights)
+
+counts = [0 for i in range(len(weights))]
+for i in range(n):
+    counts[sol.pickIndex()] += 1
+
+print(["{:.2f}%".format(c/n*100) for c in counts])
